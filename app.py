ab26ae9063eb6c6da671d1dc7030bdee5d82eadd
pacificg <prashant.kumar@meesho.com>
2022-05-20 08:47:10 +0530

    changes

--- app.py
+++ app.py
@@ -24,32 +24,36 @@ def get_img_with_href(local_img_path, target_url):
         </a>'''
     return html_code
 gif_html = get_img_with_href('super.jpg', 'https://www.supermind.design/')
-st.sidebar.markdown(gif_html, unsafe_allow_html=True)
+st.sidebar.markdown(f'<div style="text-align: center"> {gif_html} </div>', unsafe_allow_html=True)
 
 # st.sidebar.image('super.jpg',caption='Supermind Design',use_column_width=True)
-db = pd.read_csv('db_updated.csv')
+db = pd.read_csv('db_updated1.csv')
+st.header('Supermind.design database output:')
 # st.write(db)
 
 process = st.sidebar.multiselect('Process',['Sense', 'Remember','Decide','Create','Learn'])
 augmentation = st.sidebar.multiselect('Augmentation',['Connect','Curate','Collaborate','Compute'])
 module = st.sidebar.multiselect('Module',['Illuminate network',	'Incentivize',	'Feed',	'Collaborate_M'])
-group = st.sidebar.multiselect('Group',['Community',	'Market',	'Ecosystem',	'Democratic "voting"',	'Democracy'])
+group = st.sidebar.multiselect('Group',['Community',	'Market',	'Ecosystem',	'Democracy'])
 sector = st.sidebar.multiselect('Specific Sector',['Consumer / retail',	'Healthcare',	'Public sector, NGO',	
             'Manufacturing hardw., Infra',	'High Tech (software)',	'Financial services',	'Professional services',	
             'Media, telco, entertainment',	'Agriculture',	'Energy, nat. resources',	'Education and academia',	'Supply chain, real estate'])
 
 if process or augmentation or module or group or sector:
     #filter by selection in sidebar consider only those rows which have 1,2 values in all columns
-    cols = process + augmentation + module + group + sector
+    cols = process + augmentation +module+ group + sector
     # dfhat = db.iloc[np.where(db[np.where(~db[p].isna()) for p in process].contains([1,2])) & np.where(db[augmentation].contains([1,2])) & np.where(db[module].contains([1,2])) & np.where(db[group].contains([1,2])) & np.where(db[sector].contains([1,2]))]
     dfhat = db
-    for p in cols:
-        dfhat = dfhat[dfhat[p].isin([1,2])]
-    dfhat.sort_values(by=cols,ascending=False,inplace=True)
-    for row in dfhat.iterrows():
-        with st.expander(row[1]['Who / What'] + ': '+row[1]['Use case']):
-            st.markdown(', '.join([key+ ':'+ dict({1:'+',2:'++'})[int(value)] for key,value in dict(row[1][cols]).items() if value != np.nan]))
-            st.write(row[1]['Description'])
+    if len(dfhat) == 0:
+        st.write('No data found')
+    else:
+        for p in cols:
+            dfhat = dfhat[dfhat[p].isin([1,2])]
+        dfhat.sort_values(by=cols,ascending=False,inplace=True)
+        for row in dfhat.iterrows():
+            with st.expander(row[1]['Who / What'] + ': '+row[1]['Use case']):
+                st.markdown(', '.join([key+ ':'+ dict({1:'+',2:'++'})[int(value)] for key,value in dict(row[1][cols]).items() if value != np.nan]))
+                st.write(row[1]['Description'])
 
     dfhat.to_csv('db_download.csv')
     with open('db_download.csv', 'rb') as f:

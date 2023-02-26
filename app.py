763fdfd149c4f7034d9d6e7d173fddcb49ef0202
pacificg <prashant.kumar@meesho.com>
2023-02-26 18:09:59 +0530

    search

--- app.py
+++ app.py
@@ -71,6 +71,10 @@ sector = st.sidebar.multiselect('Specific Sector',['Consumer / retail',	'Healthc
 
 cols = process + [w if w != 'Collaborate ' else 'Collaborate_' for w in augmentation ] +module+ group + sector
 
+names = ['Sense', 'Remember','Decide','Create','Learn','Illuminate network',	'Incentivize',	'Feed',	'Collaborate_','Community',	'Market',	'Ecosystem',	'Democracy','Connect','Curate','Collaborate_','Compute','Consumer / retail',	'Healthcare',	'Public sector, NGO',
+         'Manufacturing hardw., Infra',	'High Tech (software)',	'Financial services',	'Professional services',	
+            'Media, telco, entertainment, hospitality',	'Agriculture',	'Energy, nat. resources',	'Education and academia',	'Supply chain, real estate' ]
+
 @st.cache(allow_output_mutation=True)
 def graph(db):
     
@@ -142,27 +146,33 @@ if button == 'Graph':
     
     
 else:
-    if process or augmentation or module or group or sector:
+    query = st.text_input('Search', value='', key=None, type='default')
+    dfhat = db
+    if query != '':
+        dfhat['Case'] = ''
+        #concatenate the column name to Who / What column where ++ or + is present
+        for col in names:
+            dfhat['Case'] = dfhat['Case'] + dfhat[col].apply(lambda x: str(col) + ' ' if x in [1,2] else '')
+        dfhat = dfhat[dfhat['Who / What'].str.contains(query, case=False) | dfhat['Use case'].str.contains(query, case=False) | dfhat['Description'].str.contains(query, case=False) | dfhat['Case'].str.contains(query, case=False)]
+
         #filter by selection in sidebar consider only those rows which have 1,2 values in all columns
-       
-        # dfhat = db.iloc[np.where(db[np.where(~db[p].isna()) for p in process].contains([1,2])) & np.where(db[augmentation].contains([1,2])) & np.where(db[module].contains([1,2])) & np.where(db[group].contains([1,2])) & np.where(db[sector].contains([1,2]))]
-        dfhat = db
-        if len(dfhat) == 0:
+        
+    # dfhat = db.iloc[np.where(db[np.where(~db[p].isna()) for p in process].contains([1,2])) & np.where(db[augmentation].contains([1,2])) & np.where(db[module].contains([1,2])) & np.where(db[group].contains([1,2])) & np.where(db[sector].contains([1,2]))]
+    for p in cols:
+        dfhat[p] = dfhat[p].apply(lambda x: float(str(x).split('"')[-1]))
+        dfhat = dfhat[dfhat[p].isin([1,2])]
+    dfhat.sort_values(by=cols,ascending=False,inplace=True)
+    if len(dfhat) == 0:
             st.write('No data found')
-        else:
-            for p in cols:
-                dfhat[p] = dfhat[p].apply(lambda x: float(str(x).split('"')[-1]))
-                dfhat = dfhat[dfhat[p].isin([1,2])]
-            dfhat.sort_values(by=cols,ascending=False,inplace=True)
-            for row in dfhat.iterrows():
-                with st.expander(row[1]['Who / What'] + ': ' + row[1]['Use case']):
-                    st.markdown(', '.join([key+ ':'+ dict({1:'+',2:'++'})[int(value)] for key,value in dict(row[1][cols]).items() if value != np.nan]))
-                    st.write(row[1]['Description'])
+    for row in dfhat.iterrows():
+        with st.expander(row[1]['Who / What'] + ': ' + row[1]['Use case']):
+            st.markdown(', '.join([key+ ':'+ dict({1:'+',2:'++'})[int(value)] for key,value in dict(row[1][cols]).items() if value != np.nan]))
+            st.write(row[1]['Description'])
         # dfhat.drop(columns=['Unnamed: 0'],inplace=True)
-        dfhat.to_csv('db_download.csv', index=False)
-        with open('db_download.csv', 'rb') as f:
-            st.sidebar.download_button('Download filtered Data', f, file_name='db_download.csv')
-        st.sidebar.write('Copyright © Supermind.design Creative Commons (share, adapt, credit) license')
+    dfhat.to_csv('db_download.csv', index=False)
+    with open('db_download.csv', 'rb') as f:
+        st.sidebar.download_button('Download filtered Data', f, file_name='db_download.csv', key=None, mime='text/csv')
+    st.sidebar.write('Copyright © Supermind.design Creative Commons (share, adapt, credit) license')
         # st.sidebar.download_link(dfhat.to_csv('db_download.csv'))
 
 
